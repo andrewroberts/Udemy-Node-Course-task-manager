adee0ca319e5ac323647040e0cf0827edb8084ae
Andrew Roberts <andrew@roberts.net>
2020-01-24 20:53:17 +0000

    sanitation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017/task-manager-api'
 const databaseName = 'task-manager'
@@ -10,18 +11,50 @@ const options = {
 
 mongoose.connect(connectionUrl, options)
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('"password" can not be used')
+            }
+        }
+
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
 
 // const me = new User({
-//     name: 'Andrew',
-//     age: 'aa'
+//     name: '   Andrew',
+//     email: '  MIKE@EMAIL.COM',
+//     password: 'hellome'
 // })
 
 // me.save().then((me) => {
@@ -32,16 +65,18 @@ mongoose.connect(connectionUrl, options)
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const task1 = new Task({
-    description: 'Task1',
-    completed: true
+    description: 'Task2',
 })
 
 task1.save().then((task1) => {
