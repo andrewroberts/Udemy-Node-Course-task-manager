b16f7465a4b4e703f4d4f77caf23dd6741252ab1
Andrew Roberts <andrew@roberts.net>
2020-01-28 18:36:04 +0000

    CRUD ops

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017/task-manager-api'
 const databaseName = 'task-manager'
@@ -10,77 +9,3 @@ const options = {
 }
 
 mongoose.connect(connectionUrl, options)
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minLength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('"password" can not be used')
-            }
-        }
-
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Invalid email')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '   Andrew',
-//     email: '  MIKE@EMAIL.COM',
-//     password: 'hellome'
-// })
-
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const task1 = new Task({
-    description: 'Task2',
-})
-
-task1.save().then((task1) => {
-    console.log(task1)
-}).catch((error) => {
-    console.log('Error!', error)
-})
---
+++ src/models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if (value.length < 7) {
+                throw new Error('The password must be 7 chars or longer')
+            }
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('"password" can not be used')
+            }
+        }
+
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+module.exports = User
